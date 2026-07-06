754d66418af84ec437070bfef5733f64a592133f
dhwangdev <98380987+dhwangdev@users.noreply.github.com>
2026-07-06 18:27:09 +0900

    Create ProfilePage.tsx

---
+++ src/pages/profile/ProfilePage.tsx
@@ -0,0 +1,47 @@
+import "./ProfilePage.css";
+
+function ProfilePage() {
+
+    // =====================
+    // State
+    // =====================
+
+
+
+    // =====================
+    // Event Handlers
+    // =====================
+
+    const handleEditProfile = () => {
+
+    };
+
+    const handleSaveProfile = () => {
+
+    };
+
+    const handleCancelEdit = () => {
+
+    };
+
+    const handleLogout = () => {
+
+    };
+
+
+    // =====================
+    // JSX
+    // =====================
+
+    return (
+
+        <>
+
+            <h1>Profile</h1>
+
+        </>
+
+    );
+}
+
+export default ProfilePage;
