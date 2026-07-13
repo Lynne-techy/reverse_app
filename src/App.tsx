b92ea386361b8c69368ae239f1ac90ae41ed722d
dhwangdev <98380987+dhwangdev@users.noreply.github.com>
2026-07-13 23:09:58 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import LoginPage from "./pages/Login/LoginPage";
 import MainPage from "./pages/MainPage/MainPage";
