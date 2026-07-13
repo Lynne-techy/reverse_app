fcba73633394f86061ec62fdeae6eb94689e2e33
dhwangdev <98380987+dhwangdev@users.noreply.github.com>
2026-07-13 22:49:52 +0900

    Update MainPage.tsx

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,52 +1,10 @@
 import "./MainPage.css";
 
-import Header from "../components/Header";
-import TodayVerse from "../components/TodayVerse";
-import ProgressCard from "../components/ProgressCard";
-import RecentRecords from "../components/RecentRecords";
+
 
 function MainPage() {
 
-    // =====================
-    // State
-    // =====================
 
-
-
-    // =====================
-    // Event Handlers
-    // =====================
-
-    const handleStartStudy = () => {
-
-    };
-
-    const handleViewProfile = () => {
-
-    };
-
-    const handleLogout = () => {
-
-    };
-
-
-    // =====================
-    // JSX
-    // =====================
-
-    return (
-        <>
-
-            <Header />
-
-            <TodayVerse />
-
-            <ProgressCard />
-
-            <RecentRecords />
-
-        </>
-    );
 }
 
 export default MainPage;
